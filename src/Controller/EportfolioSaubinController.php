d7bf95deeaffe8e98b690301f52ef9dc2ac93a0d
Titouan SAUBIN <titouan.saubin@etu.univ-st-etienne.fr>
2024-01-15 22:20:07 +0100

    V1

--- src/Controller/EportfolioSaubinController.php
+++ src/Controller/EportfolioSaubinController.php
@@ -8,11 +8,25 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class EportfolioSaubinController extends AbstractController
 {
-    #[Route('/eportfolio/saubin', name: 'app_eportfolio_saubin')]
+    #[Route('/', name: 'app_eportfolio')]
     public function index(): Response
     {
         return $this->render('eportfolio_saubin/index.html.twig', [
-            'controller_name' => 'EportfolioSaubinController',
+            'controller_name' => 'EportfolioSaubin',
+        ]);
+    }
+    #[Route('/descri', name: 'app_descri')]
+    public function home() 
+    {
+        return $this->render('eportfolio_saubin/description.html.twig',[
+            'Titre' => 'E-Portfolio',
+        ]);
+    }
+    #[Route('/cv', name: 'app_cv')]
+    public function suite() 
+    {
+        return $this->render('eportfolio_saubin/cv.html.twig',[
+            'Titre' => "Ce que je suis et ce que j'apprécie",
         ]);
     }
 }
