cf9f9bc036d08296a68cbca80816754a268e00d4
Titouan SAUBIN <titouan.saubin@etu.univ-st-etienne.fr>
2024-01-16 18:33:57 +0100

    v2Index Fini

--- src/Controller/EportfolioSaubinController.php
+++ src/Controller/EportfolioSaubinController.php
@@ -12,21 +12,55 @@ class EportfolioSaubinController extends AbstractController
     public function index(): Response
     {
         return $this->render('eportfolio_saubin/index.html.twig', [
-            'controller_name' => 'EportfolioSaubin',
+            'controller_name' => 'E-eportfolio',
         ]);
     }
+
     #[Route('/descri', name: 'app_descri')]
     public function home() 
     {
         return $this->render('eportfolio_saubin/description.html.twig',[
-            'Titre' => 'E-Portfolio',
+            'Titre' => "Ce que je suis et ce que j'apprécie",
         ]);
     }
+
     #[Route('/cv', name: 'app_cv')]
     public function suite() 
     {
         return $this->render('eportfolio_saubin/cv.html.twig',[
-            'Titre' => "Ce que je suis et ce que j'apprécie",
+            'Poste' => "Administrateur réseau",
+        ]);
+    }
+
+    #[Route('/Admini', name: 'app_Admini')]
+    public function Compet1() 
+    {
+        return $this->render('eportfolio_saubin/Competence/Admini.html.twig',[
+            'Titre' => "Administrer les Réseaux et l'Internet",
+        ]);
+    }
+
+    #[Route('/Connect', name: 'app_Connect')]
+    public function Compet2() 
+    {
+        return $this->render('eportfolio_saubin/Competence/Connect.html.twig',[
+            'Titre' => "Connecter les Entreprises et les Usagers",
+        ]);
+    }
+
+    #[Route('/Prog', name: 'app_Prog')]
+    public function Compet3() 
+    {
+        return $this->render('eportfolio_saubin/Competence/Prog.html.twig',[
+            'Titre' => "Créer des Outils et Applications informatiques pour les R&T",
+        ]);
+    }
+
+    #[Route('/Plus', name: 'app_Plus')]
+    public function Plus() 
+    {
+        return $this->render('eportfolio_saubin/Competence/Plus.html.twig',[
+            'Titre' => "Autres",
         ]);
     }
 }
